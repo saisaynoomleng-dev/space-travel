6e7ff79bf7e557ea63d4717bc9733040b2306672
saisaynoomleng-dev <snoomleng1@cougars.ccis.edu>
2025-08-06 16:27:35 -0500

    finish setting up

---
+++ src/app/(frontend)/page.tsx
@@ -0,0 +1,11 @@
+import Bounded from '@/components/Bounded';
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <Bounded>
+      <h1>Hello Space</h1>
+      <Button>Select</Button>
+    </Bounded>
+  );
+}
